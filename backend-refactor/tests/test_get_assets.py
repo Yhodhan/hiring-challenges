e801244840a054c4f8a39de50c02c6ab3abbe2ba
Yhodhan <mscuderi855@gmail.com>
2026-02-06 19:15:16 +0100

    Fix requirements dependencies issues for testing, add simple tests for Assets

--- backend-refactor/tests/test_get_assets.py
+++ backend-refactor/tests/test_get_assets.py
@@ -1,6 +1,26 @@
 from fastapi.testclient import TestClient
+from app.services.asset import AssetService
 
 from app.main import app
 
+client = TestClient(app)
+
 def test_get_todo():
-    assert True
+
+    response = client.get("api/v1/assets")
+
+    assert response.status_code == 200
+    assert response.json()[0]["asset_id"] == "1"
+
+def test_get_nothing():
+
+    class MockAsset:
+        def get_all_assets(self):
+            return []
+
+    client.app.dependency_overrides[AssetService] = MockAsset
+
+    response = client.get("api/v1/assets")
+
+    assert response.status_code == 200
+    assert response.json() == []
