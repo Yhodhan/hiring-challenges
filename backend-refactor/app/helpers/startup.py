6d382bee7ff9878578aa718974995875acc189a6
Yhodhan <mscuderi855@gmail.com>
2026-02-06 14:53:58 +0100

    Rename app.py to factory.py to match the Factory design pattern, remove unneeded startup.py file, moves its logic to factory

--- backend-refactor/app/helpers/startup.py
+++
@@ -1,9 +0,0 @@
-from app.services import measurement_legacy
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-
-def startup(app):   
-    # Mount static files
-    app.mount("/static", StaticFiles(directory="app/static"), name="static")
-    app.include_router(measurement_legacy.router, tags=["measurements"])
