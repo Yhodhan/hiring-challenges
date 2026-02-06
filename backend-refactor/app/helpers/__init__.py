59660e4cd3da507c324feec82b8fde12febe84b6
Yhodhan <mscuderi855@gmail.com>
2026-02-06 15:57:15 +0100

    remove unused helpers package

--- backend-refactor/app/helpers/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Helpers package."""
-from app.helpers.signal_helper import get_signal_by_id, GetSignalById, find_signal, filter_signals_by_asset, FilterSignalsByAsset
-from app.helpers.asset_helper import group_signals_by_asset, GroupSignalsByAsset
-
-__all__ = [
-    "get_signal_by_id", "GetSignalById", "find_signal", "filter_signals_by_asset", "FilterSignalsByAsset",
-    "group_signals_by_asset", "GroupSignalsByAsset"
-]
